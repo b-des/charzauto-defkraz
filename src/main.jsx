4f70bfce1154d454b5bcd562579fc18937746110
Andrii <alltelex@gmail.com>
2026-08-04 17:48:52 +0300

    Add openapi generator for http client

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import VehicleRepairComponent from './App.jsx'
-import { registerSW } from 'virtual:pwa-register'
+import {registerSW} from 'virtual:pwa-register'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-registerSW({ immediate: true })
+registerSW({immediate: true})
 
+export const queryClient = new QueryClient({
 
+    defaultOptions: {
+
+        queries: {
+
+            retry: 0,
+
+            staleTime: 5 * 60 * 1000,
+
+            gcTime: 10 * 60 * 1000,
+
+            refetchOnWindowFocus: false,
+
+        },
+
+    },
+
+});
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <VehicleRepairComponent />
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <VehicleRepairComponent/>
+        </QueryClientProvider>
+    </StrictMode>,
 )
